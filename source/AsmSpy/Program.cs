1542e0711f0328e267b5e7c848e4bb5ee1a0c5d1
aledesma <anthony.ledesma@firehost.com>
2014-05-09 21:28:40 -0500

    Move projects into source directory

---
+++ source/AsmSpy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using CommandLine;
+
+namespace AsmSpy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = GetOptions(args);
+            var spy = new AsmSpy(options.All, options.Path, options.SkipSystem, options.SubDirectories, Console.Out);
+            spy.Analyse();
+
+            if(Debugger.IsAttached)
+                Debugger.Break();
+        }
+
+        private static Options GetOptions(string[] args)
+        {
+            var parser = new Parser(x =>
+            {
+                x.HelpWriter = Console.Error;
+                x.CaseSensitive = false;
+                x.IgnoreUnknownArguments = false;
+                x.MutuallyExclusive = false;
+                x.ParsingCulture = CultureInfo.CurrentCulture;
+            });
+
+            var options = new Options();
+            if (!parser.ParseArguments(args, options))
+            {
+                Environment.Exit(Parser.DefaultExitCodeFail);
+            }
+
+            return options;
+        }
+    }
+}
