9e43ee24c9a699bdd640e560dacb7cec92646793
aledesma <anthony.ledesma@firehost.com>
2014-05-09 19:08:12 -0500

    Add DirectoryInfoExtensions

---
+++ AsmSpy/DirectoryInfoExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AsmSpy
+{
+    internal static class DirectoryInfoExtensions
+    {
+        public static IEnumerable<FileInfo> GetFilesByExtensions(this DirectoryInfo dir, params string[] extensions)
+        {
+            return GetFilesByExtensions(dir, SearchOption.TopDirectoryOnly, extensions);
+        }
+
+        public static IEnumerable<FileInfo> GetFilesByExtensions(this DirectoryInfo dir, SearchOption searchOption, params string[] extensions)
+        {
+            if(extensions == null)
+                throw new ArgumentNullException("extensions");
+
+            return extensions.Aggregate(Enumerable.Empty<FileInfo>(), (current, ext) => current.Concat(dir.GetFiles(ext, searchOption)));
+        }
+    }
+}
